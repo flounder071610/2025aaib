9163aedffcd6d3bf172a49f4388ec881a69ac0d3
flounder071610 <ayakaflounder@gmail.com>
2025-02-19 11:55:44 +0800

    Add files via upload

---
+++ week01.cpp
@@ -0,0 +1,17 @@
+///week01瘋狂程設 兩數相加
+///下週考這題
+///Ctrl-S存檔成week01-1.cpp
+#include <stdio.h>///檔名不能寫錯
+///stido的意思是標準的standard input/output
+///有了stido.h的外掛，才能用scanf() printf()
+
+int main()
+{
+	int a, b;///整數a,b
+	scanf( "%d" , &a );///掃描讀東西
+	scanf( "%d" , &b );///同上
+
+	printf("%d" , a+b );///印東西
+
+
+}
